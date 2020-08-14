c1b454bc1335f1c0b9c4c315a90019304ff94643
Matthew Gould <mldgould@gmail.com>
2020-08-14 18:23:32 +1000

    Added | Completed banner

--- projects/vending/src/app/containers/banner/banner.component.ts
+++ projects/vending/src/app/containers/banner/banner.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'sm-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class BannerComponent {}
