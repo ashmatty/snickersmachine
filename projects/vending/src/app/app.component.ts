ce164b113af77a10861533f4d796a6470e8f729d
Matthew Gould <mldgould@gmail.com>
2020-08-16 09:34:58 +1000

    Added | Initial supply for app init.

--- projects/vending/src/app/app.component.ts
+++ projects/vending/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import * as reducers from './store/reducers';
+import * as supplyActions from './store/actions/supply.action';
 import * as supplySelectors from './store/selectors/supply.selector';
 
 import { COLUMN, VendLocation } from './models';
@@ -12,13 +13,18 @@ import { COLUMN, VendLocation } from './models';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(private _store: Store<reducers.VendingMachineState>){}
-
+export class AppComponent implements OnInit {
   supply$: Observable<number> = this._store.select(supplySelectors.getSupply);
 
   testOrder: VendLocation = {
     row: 'B',
     column: COLUMN.LEFT,
+  };
+
+  constructor(private _store: Store<reducers.VendingMachineState>) {}
+
+  ngOnInit() {
+    // Initialise the supply with 2 Snickers bars.
+    this._store.dispatch(new supplyActions.Supply(2));
   }
 }
