45e8b91354caeeb623e90231e01e80ab0ad9ae9a
Matthew Gould <mldgould@gmail.com>
2020-08-15 20:13:46 +1000

    Fixed | Fixed shelf component.

--- projects/vending/src/app/components/vending-machine/shelf/shelf.component.ts
+++ projects/vending/src/app/components/vending-machine/shelf/shelf.component.ts
@@ -1,11 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'sm-shelf',
   templateUrl: './shelf.component.html',
   styleUrls: ['./shelf.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class ShelfComponent {
+export class ShelfComponent implements OnChanges{
   @Input()
   rowSupply: { left: number, right: number };
+
+  ngOnChanges(changes) {
+    console.log(changes);
+  }
 }
